8db13992dbc10fd6951b472d7362444e92cf00d2
Ynda Jas <yndajas@gmail.com>
2020-07-06 03:06:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(i)
+  if i.between?(-1,1)
+    false
+  elsif i == -2 || i == 2
+    true
+  else
+    if i < 0
+      base = -2
+    else
+      base = 2
+    end
+  previous_numbers = (base..(i-1)).to_a
+  previous_numbers.none? {|prev|i % prev == 0}
+  end
+end
